bc3a7bb63e1382dd67af9726ab2d370c59b8077f
Himanshu Bhatt <hbhatt034@gmail.com>
2023-12-03 11:32:45 +0530

    Added flights search crud with few filters

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -26,6 +26,16 @@ class FlightService {
       throw { error };
     }
   }
+
+  async getAllFlightData(data) {
+    try {
+      const flights = await this.flightRepository.getAllFlight(data);
+      return flights;
+    } catch (error) {
+      console.log("Something went wrong at service layer");
+      throw { error };
+    }
+  }
 }
 
 module.exports = FlightService;
